87df1931d09a16da0b0d37bebde6a04d5f6aa01a
Anny <anny@Annys-MBP.netis.cc>
2023-09-23 18:43:25 +0400

    homework done

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+ void PrintMax(int arg1, int arg2)
+ {
+     int max;
+
+     if (arg1 > arg2)
+     {
+     max = arg1;
+     }
+     else
+     {
+         max = arg2;
+     }
+     Console.WriteLine(max);
+ }
+ PrintMax(5,7);
+ PrintMax(2,10);
+ PrintMax(-9,-3);
---
+++ homework 1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+ 
+ void PrintEvenNumbers(int n)
+ {
+     int index = 1;
+
+     while (index <= n)
+     {
+         if (index % 2 == 0)
+         {
+             Console.WriteLine(index);
+         }
+         index++;
+     }
+ }
+ PrintEvenNumbers(5);
+ PrintEvenNumbers(8);
